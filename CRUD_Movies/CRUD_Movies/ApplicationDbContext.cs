d854badcdaf17cce300dcd5a2a2f133595751396
AElomda <elomdaa7444@gmail.com>
2024-09-22 19:25:46 +0300

    Create Project and Add Entity Framework

---
+++ CRUD_Movies/CRUD_Movies/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Movies
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=CRUD_Movies;Trusted_Connection=True;Encrypt=false");
+        }
+    }
+}
---
+++ CRUD_Movies/CRUD_Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
